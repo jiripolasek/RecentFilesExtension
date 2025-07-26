084543dee3639781ea1fb54618616f5f2b9e39fa
Jiří Polášek <me@jiripolasek.com>
2025-07-26 15:07:23 +0200

    Upgrade JPSoftworks.CommandPalette.Extensions.Toolkit to 0.3.0 (#17)
    * Switch to new ExtensionHostRunner in Toolkit 0.3.0
    
    * Upgrade package JPSoftworks.CommandPalette.Extensions.Toolkit

--- src/JPSoftworks.RecentFilesExtension/Program.cs
+++ src/JPSoftworks.RecentFilesExtension/Program.cs
@@ -4,9 +4,7 @@
 //
 // ------------------------------------------------------------
 
-using JPSoftworks.CommandPalette.Extensions.Toolkit.Helpers;
-using Shmuelie.WinRTServer;
-using Shmuelie.WinRTServer.CsWinRT;
+using JPSoftworks.CommandPalette.Extensions.Toolkit;
 
 namespace JPSoftworks.RecentFilesExtension;
 
@@ -15,30 +13,17 @@ public static class Program
     [MTAThread]
     public static async Task Main(string[] args)
     {
-        Logger.Initialize("JPSoftworks", "RecentFilesExtension");
-
-        if (args.Length > 0 && args[0] == "-RegisterProcessAsComServer")
+        await ExtensionHostRunner.RunAsync(args, new()
         {
-            ComServer server = new();
-            ManualResetEvent extensionDisposedEvent = new(false);
-
-            // We are instantiating an extension instance once above, and returning it every time the callback in RegisterExtension below is called.
-            // This makes sure that only one instance of SampleExtension is alive, which is returned every time the host asks for the IExtension object.
-            // If you want to instantiate a new instance each time the host asks, create the new instance inside the delegate.
-            RecentFilesExtension extensionInstance = new(extensionDisposedEvent);
-            server.RegisterClass<RecentFilesExtension, IExtension>(() => extensionInstance);
-            server.Start();
-
-            // This will make the main thread wait until the event is signaled by the extension class.
-            // Since we have single instance of the extension object, we exit as soon as it is disposed.
-            extensionDisposedEvent.WaitOne();
-
-            // Bye, bye
-            server.UnsafeDispose();
-        }
-        else
-        {
-            await StartupHelper.HandleDirectLaunchAsync();
-        }
+            PublisherMoniker = "JPSoftworks",
+            ProductMoniker = "RecentFilesExtension",
+            EnableEfficiencyMode = true,
+            ExtensionFactories = [
+                new DelegateExtensionFactory(extensionDisposedEvent => new RecentFilesExtension(extensionDisposedEvent))
+            ],
+#if DEBUG
+            IsDebug = true,
+#endif
+        });
     }
 }
